deb704777fc563d369705567eded74613622042f
Adair Hernandez <adheva05@gmail.com>
2024-09-06 03:14:12 -0600

    tercer commit

--- Hello world.py
+++ Hello world.py
@@ -2,7 +2,10 @@ print("Hello world")
 
 print("Suma")
 suma = 0
+multi = 0
 a = 1
 b = 3
 suma = a+b
 print("La suma de",a,"+",b,"es: ", suma)
+multi = a*b
+print("La multiplicacion de",a,"*",b,"es: ", multi)
